6cc0a6f47504e66f12938975ded07cf4a0c3bdff
who is this <standoffeon@gmail.com>
2026-08-20 14:20:42 +0300

    Update battleye.lua

--- battleye.lua
+++ battleye.lua
@@ -1,3 +1,4 @@
+local yeah=[[
 task.wait(0.15)
 local G2L = {};
 const function ha()
@@ -12,6 +13,7 @@ messagebox(
 )
 end 
 ha()
+queueonteleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/whoisthisxor/assets/refs/heads/main/battleye.lua"))()')
 local pgui=game:GetService("CoreGui")
 -- StarterGui.
 G2L["1"] = Instance.new("ScreenGui",pgui)
@@ -53,6 +55,7 @@ G2L["4"]["Text"] = "You Have Been Detected With Cheating"
 G2L["4"]["Position"] = UDim2.new(0.30016, 0, -0.01132, 0);
 
 
+-- StarterGui..main.2
 G2L["5"] = Instance.new("TextLabel", G2L["2"]);
 G2L["5"]["BorderSizePixel"] = 0;
 G2L["5"]["TextSize"] = 32;
@@ -75,8 +78,9 @@ G2L["6"]["Size"] = UDim2.new(0, 3038, 0, 1895);
 G2L["6"]["Position"] = UDim2.new(-0.71065, 0, -0.71131, 0);
 G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
 G2L["6"]["Name"] = "black"
-getgenv().isnewcclosure = nil 
-getgenv().clearteleportqueue = newcclosure(function(...) return "Unable To Clear Teleport Queue",false end)
 
 
 return G2L["1"], require;
+
+]]
+local haa=loadstring(yeah)()
